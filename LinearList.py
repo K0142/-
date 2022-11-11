13120de7a981c4f6075772bf958da66cd2bb7183
K0142 <107910483+K0142@users.noreply.github.com>
2022-11-11 15:29:55 +0900

    선형 리스트

--- LinearList.py
+++ LinearList.py
@@ -9,14 +9,11 @@ def insert_data(position, animal):
 	if position <0 or position > len(animal_list):
 		print("데이터 삽입 범위를 벗어났습니다.")
 		return
-
 	animal_list.append(None) 
 	list_len = len(animal_list)
-
 	for i in range(list_len - 1, position, -1):
 		animal_list[i] = animal_list[i-1]
 		animal_list[i-1] = None
-	
 	animal_list[position] = animal
 
 # 데이터 삭제
@@ -24,24 +21,19 @@ def delete_data(position):
 	if position <0 or position > len(animal_list):
 		print("데이터 삭제 범위를 벗어났습니다.")
 		return
-
 	list_len = len(animal_list)
 	animal_list[position] = None
-
 	for i in range(position+1, list_len):
 		animal_list[i-1] = animal_list[i]
 		animal_list[i] = None
-
 	del(animal_list[list_len-1])
 
 # 변수 선언
 animal_list = []
 select = -1      # 1: 추가 2: 삽입 3: 삭제 4: 종료
-
 if __name__ == "__main__":
 	while(select !=4):
 		select = int(input("선택하세요(1: 추가 2: 삽입 3: 삭제 4: 종료) ==>"))
-
 		if(select==1):
 			data = input("추가할 데이터 ==>")
 			add_data(data)

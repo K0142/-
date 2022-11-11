e80fc52ccf4588af3ed81807e237ddd0029595d4
K0142 <107910483+K0142@users.noreply.github.com>
2022-11-11 19:36:40 +0900

    Prim

--- Prim.py
+++ Prim.py
@@ -32,7 +32,7 @@ vertex_list = ["A", "B", "C", "D", "E", "F"]
 weight = [ [None, 29  , None, None, None, 10  , None],
            [29  , None, 16  , None, None, None, 15  ],
            [None, 16  , None, 12  , None, None, None],
-           [None, None, 12  , None, 22  , None, 18  ],
+           [None, None, 12  , None, 22  , None, 18  ], 
            [None, None, None, 22  , None, 27  , 25  ],
            [10  , None, None, None, 27  , None, None],
            [None, 15  , None, 18  , 25  , None, None]]

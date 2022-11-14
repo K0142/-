6d2c38a8377a986ee08a8947dd4b031e7de2e88e
K1042 <spaceof42@gmail.com>
2022-11-14 16:17:53 +0900

    Quick Sort

--- sort/quick_sort.py
+++ sort/quick_sort.py
@@ -7,14 +7,15 @@ def quick_sort(array, start, end):
 	left = start + 1
 	right = end
 	while(left <= right):
-		# pivot보다 큰 데이터 찾을 때까지
+		# pivot보다 큰 데이터를 찾을 때까지
 		while(left <= end and array[left] <= array[pivot]):
 			left += 1
+		# pivot보다 작은 데이터를 찾을 때까지
 		while(right > start and array[right] >= array[pivot]):
 			right -= 1
-		if(left > right):
+		if(left > right):  # 엇갈렸다면 작은 데이터와 pivot을 교환
 			array[right], array[pivot] = array[pivot], array[right]
-		else:
+		else:              # 엇갈리지 않았다면 작은 데이터와 큰 데이터를 교환
 			array[left], array[right] = array[right], array[left]
 		
 		quick_sort(array, start, right - 1)
